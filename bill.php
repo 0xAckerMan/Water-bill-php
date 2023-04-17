50f0d83c51f9c59293ed054d7758d9cf6b36c548
Kirui05 <126835183+Kirui05@users.noreply.github.com>
2023-04-17 16:41:33 +0300

    bill.php

--- bill.php
+++ bill.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Calculate Monthly Water Bill</title>
-</head>
-<body>
-	<h1>Calculate Monthly Water Bill</h1>
-	<form method="POST">
-		<label for="units">Enter number of units:</label>
-		<input type="text" name="units" required><br><br>
-		<input type="submit" name="submit" value="Calculate Bill">
-	</form>
+
 	<?php
 	if(isset($_POST['submit'])) {
 		$units = $_POST['units'];
@@ -32,8 +21,7 @@
 		echo "<h3>Your monthly water bill is Ksh. " . $bill . "</h3>";
 	}
 	?>
-</body>
-</html>
+
 
 
 
